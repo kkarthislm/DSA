33c37c2ff4fd09a91260662ac84d20bdb6cbf93c
Karthick <kkarthislm@gmail.com>
2020-08-13 22:02:17 +0530

    initial code commit

---
+++ src/array/StaticArray.java
@@ -0,0 +1,35 @@
+package array;
+
+public class StaticArray {
+
+	public static void main(String[] args) {
+
+	}
+	
+}
+
+class Array<T>{
+
+	@SuppressWarnings("unused")
+	private Array() {
+	}
+	
+	public Array(int n) {
+		super();
+	}
+
+	T add(T t){
+		return null;
+		
+	}
+	
+	T edit(int i, T t){
+		return null;
+		
+	}
+	
+	int search(){
+		return 0;
+		
+	}
+}
